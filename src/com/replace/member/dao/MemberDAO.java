65f1c80e30e23d58ea56aac59c4b9a76e801a78a
alert-Enoch <alert4739@gmail.com>
2023-07-07 17:10:08 +0900

    추가적인 변동사항 업로드

---
+++ src/com/replace/member/dao/MemberDAO.java
@@ -0,0 +1,62 @@
+package com.replace.member.dao;
+
+import java.util.HashMap;
+
+import org.apache.ibatis.session.SqlSession;
+
+import com.replace.mybatis.config.MyBatisConfig;
+import com.replace.member.domain.MemberVO;
+
+public class MemberDAO {
+	public SqlSession sqlSession;
+	
+	public MemberDAO() {
+		sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
+	}
+	/*	
+//	회원가입
+	public void insert(MemberVO memberVO) {
+		sqlSession.insert("member.insert", memberVO);
+	}
+		
+//	아이디 중복검사
+	public String selectIdentification(String memberIdentification) {
+		return sqlSession.selectOne("member.selectIdentification", memberIdentification);
+	}
+	
+//	이메일 중복검사
+	public String selectEmail(String memberEmail) {
+		return sqlSession.selectOne("member.selectEmail", memberEmail);
+	}
+	
+//	로그인
+	public Long login(String memberIdentification, String memberPassword) {
+		HashMap<String, String> loginMap = new HashMap<String, String>();
+		loginMap.put("memberIdentification", memberIdentification);
+		loginMap.put("memberPassword", memberPassword);
+		
+		return sqlSession.selectOne("member.login", loginMap);
+	}*/
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/replace/member/domain/MemberVO.java
@@ -0,0 +1,107 @@
+package com.replace.member.domain;
+
+public class MemberVO {
+	// 객체 선언
+//	private Long memberId;
+//	private String memberIdentification;
+//	private String memberPassword;
+//	private String memberName;
+//	private String memberBirth;
+//	private String memberPhone;
+//	private String memberEmail;
+	
+	//기본 생성자 선언
+	// getter, setter, toString 선언
+	// hashCode, equals 재정의
+	
+	/* public MemberVO() {;} */
+
+
+//	public Long getMemberId() {
+//		return memberId;
+//	}
+//
+//	public void setMemberId(Long memberId) {
+//		this.memberId = memberId;
+//	}
+//
+//	public String getMemberIdentification() {
+//		return memberIdentification;
+//	}
+//
+//	public void setMemberIdentification(String memberIdentification) {
+//		this.memberIdentification = memberIdentification;
+//	}
+//
+//	public String getMemberPassword() {
+//		return memberPassword;
+//	}
+//
+//	public void setMemberPassword(String memberPassword) {
+//		this.memberPassword = memberPassword;
+//	}
+//
+//	public String getMemberName() {
+//		return memberName;
+//	}
+//
+//	public void setMemberName(String memberName) {
+//		this.memberName = memberName;
+//	}
+//
+//	public String getMemberBirth() {
+//		return memberBirth;
+//	}
+//
+//	public void setMemberBirth(String memberBirth) {
+//		this.memberBirth = memberBirth;
+//	}
+//
+//	public String getMemberPhone() {
+//		return memberPhone;
+//	}
+//
+//	public void setMemberPhone(String memberPhone) {
+//		this.memberPhone = memberPhone;
+//	}
+//
+//	public String getMemberEmail() {
+//		return memberEmail;
+//	}
+//
+//	public void setMemberEmail(String memberEmail) {
+//		this.memberEmail = memberEmail;
+//	}
+//
+//	@Override
+//	public String toString() {
+//		return "MemberVO [memberId=" + memberId + ", memberIdentification=" + memberIdentification + ", memberPassword="
+//				+ memberPassword + ", memberName=" + memberName + ", memberBirth=" + memberBirth + ", memberPhone="
+//				+ memberPhone + ", memberEmail=" + memberEmail + "]";
+//	}
+//
+//	@Override
+//	public int hashCode() {
+//		final int prime = 31;
+//		int result = 1;
+//		result = prime * result + ((memberId == null) ? 0 : memberId.hashCode());
+//		return result;
+//	}
+//
+//	@Override
+//	public boolean equals(Object obj) {
+//		if (this == obj)
+//			return true;
+//		if (obj == null)
+//			return false;
+//		if (getClass() != obj.getClass())
+//			return false;
+//		MemberVO other = (MemberVO) obj;
+//		if (memberId == null) {
+//			if (other.memberId != null)
+//				return false;
+//		} else if (!memberId.equals(other.memberId))
+//			return false;
+//		return true;
+//	}
+}
